74cc816d33735caf36d32994f4d9e1de506353b6
admin <admin@DESKTOP-9NITIJV>
2025-07-04 11:06:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter num1");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("enter num1");
+        //    int b = Convert.ToInt32(Console.ReadLine());
+        //    int sum;
+        //    sum = a + b;
+        //    Console.WriteLine("sum is: " + sum);
+        //}
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
