e144e6007a01279352581b521858dc420daac9b3
HimaniSolanki1812 <himanisolanki1812@gmail.com>
2025-07-24 21:32:50 +0530

    24-7-2025

--- Program14.cs
+++ Program14.cs
@@ -10,26 +10,40 @@ namespace Project_BD
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("23SOECE11079  -- Solanki Himani");
-            Console.Write("Enter Number: ");
-            string number = Console.ReadLine();
+            //Console.WriteLine("23SOECE11079  -- Solanki Himani");
+            //Console.Write("Enter Number: ");
+            //string number = Console.ReadLine();
 
-            foreach (char digit in number)
+            //foreach (char digit in number)
+            //{
+            //    switch (digit)
+            //    {
+            //        case '0': Console.Write("Zero "); break;
+            //        case '1': Console.Write("One "); break;
+            //        case '2': Console.Write("Two "); break;
+            //        case '3': Console.Write("Three "); break;
+            //        case '4': Console.Write("Four "); break;
+            //        case '5': Console.Write("Five "); break;
+            //        case '6': Console.Write("Six "); break;
+            //        case '7': Console.Write("Seven "); break;
+            //        case '8': Console.Write("Eight "); break;
+            //        case '9': Console.Write("Nine "); break;
+            //    }
+            //}
+            Console.WriteLine("enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string[] dig = { "zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine " };
+            string result = null;
+            while (num > 0)
             {
-                switch (digit)
-                {
-                    case '0': Console.Write("Zero "); break;
-                    case '1': Console.Write("One "); break;
-                    case '2': Console.Write("Two "); break;
-                    case '3': Console.Write("Three "); break;
-                    case '4': Console.Write("Four "); break;
-                    case '5': Console.Write("Five "); break;
-                    case '6': Console.Write("Six "); break;
-                    case '7': Console.Write("Seven "); break;
-                    case '8': Console.Write("Eight "); break;
-                    case '9': Console.Write("Nine "); break;
-                }
+                int digit = num % 10;
+                num = num / 10;
+                result = dig[digit] + result;
+
+
             }
+            Console.WriteLine(result);
+
         }
     }
 }
